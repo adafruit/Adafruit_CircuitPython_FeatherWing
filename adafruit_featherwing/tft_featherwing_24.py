4748599196defcd01f66b978f80527f026adb0a7
foamyguy <foamyguy@gmail.com>
2021-03-15 21:03:12 -0500

    refactor TFT Featherwings to use base class for avoiding duplicate

--- adafruit_featherwing/tft_featherwing_24.py
+++ adafruit_featherwing/tft_featherwing_24.py
@@ -20,41 +20,21 @@ Requires:
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_FeatherWing.git"
 
-import board
-import digitalio
-import displayio
+
 import adafruit_ili9341
-from adafruit_stmpe610 import Adafruit_STMPE610_SPI
-import sdcardio
-import storage
 
 # pylint: disable-msg=too-few-public-methods
-class TFTFeatherWing24:
+from adafruit_featherwing.tft_featherwing import TFTFeatherWing
+
+
+class TFTFeatherWing24(TFTFeatherWing):
     """Class representing an `TFT FeatherWing 2.4
     <https://www.adafruit.com/product/3315>`_.
 
     """
 
     def __init__(self, spi=None, cs=None, dc=None):
-        displayio.release_displays()
-        if spi is None:
-            spi = board.SPI()
-        if cs is None:
-            cs = board.D9
-        if dc is None:
-            dc = board.D10
-
-        ts_cs = digitalio.DigitalInOut(board.D6)
-        self.touchscreen = Adafruit_STMPE610_SPI(spi, ts_cs)
-
-        display_bus = displayio.FourWire(spi, command=dc, chip_select=cs)
-        self.display = adafruit_ili9341.ILI9341(display_bus, width=320, height=240)
-
-        sd_cs = board.D5
-        self._sdcard = None
-        try:
-            self._sdcard = sdcardio.SDCard(spi, sd_cs)
-            vfs = storage.VfsFat(self._sdcard)
-            storage.mount(vfs, "/sd")
-        except OSError as error:
-            print("No SD card found:", error)
+        super().__init__(spi, cs, dc)
+        self.display = adafruit_ili9341.ILI9341(
+            self._display_bus, width=320, height=240
+        )
--- adafruit_featherwing/tft_featherwing_35.py
+++ adafruit_featherwing/tft_featherwing_35.py
@@ -20,41 +20,18 @@ Requires:
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_FeatherWing.git"
 
-import board
-import digitalio
-import displayio
 from adafruit_hx8357 import HX8357
-from adafruit_stmpe610 import Adafruit_STMPE610_SPI
-import sdcardio
-import storage
 
 # pylint: disable-msg=too-few-public-methods
-class TFTFeatherWing35:
+from adafruit_featherwing.tft_featherwing import TFTFeatherWing
+
+
+class TFTFeatherWing35(TFTFeatherWing):
     """Class representing an `TFT FeatherWing 3.5
     <https://www.adafruit.com/product/3651>`_.
 
     """
 
     def __init__(self, spi=None, cs=None, dc=None):
-        displayio.release_displays()
-        if spi is None:
-            spi = board.SPI()
-        if cs is None:
-            cs = board.D9
-        if dc is None:
-            dc = board.D10
-
-        ts_cs = digitalio.DigitalInOut(board.D6)
-        self.touchscreen = Adafruit_STMPE610_SPI(spi, ts_cs)
-
-        display_bus = displayio.FourWire(spi, command=dc, chip_select=cs)
-        self.display = HX8357(display_bus, width=480, height=320)
-
-        sd_cs = board.D5
-        self._sdcard = None
-        try:
-            self._sdcard = sdcardio.SDCard(spi, sd_cs)
-            vfs = storage.VfsFat(self._sdcard)
-            storage.mount(vfs, "/sd")
-        except OSError as error:
-            print("No SD card found:", error)
+        super().__init__(spi, cs, dc)
+        self.display = HX8357(self._display_bus, width=480, height=320)
